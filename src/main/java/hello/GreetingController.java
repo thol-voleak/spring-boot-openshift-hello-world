f858c47a59ee802c9419a4c69324110a3ebff5a0
thol-voleak <voleak.rupp@gmail.com>
2018-03-22 16:21:55 +0700

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -21,6 +21,19 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @ApiOperation(value = "getGreeting", nickname = "getGreeting")
+    @RequestMapping(method = RequestMethod.GET, path = "/healthz", produces = "application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = Greeting.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")})
+    public String getHealthz() {
+        return "ok";
+    }
+    
+    
+    @ApiOperation(value = "getGreeting", nickname = "getGreeting")
     @RequestMapping(method = RequestMethod.GET, path = "/", produces = "application/json")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "name", value = "User's name", required = false, dataType = "string", paramType = "query", defaultValue = "Andy")
