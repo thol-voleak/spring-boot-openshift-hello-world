a4bf286730bdb088164b3aa6fa36e076308ca605
thol-voleak <voleak.rupp@gmail.com>
2018-07-24 10:46:01 +0700

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -5,7 +5,7 @@ package hello;
  * Date: 2016-08-21
  * Time: 00:12
  */
-
+import javax.servlet.http.HttpServletRequest;
 import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
